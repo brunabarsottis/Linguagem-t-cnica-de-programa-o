de168db83e921a8cfef980751dfd93d97312da9f
brunabarsottis <brunabarsottis@gmail.com>
2026-08-04 09:09:06 -0300

    Create Atv 1 soma.c

---
+++ Atv 1 soma.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int a, b, c;
+	
+	a = 10;
+	b = 5;
+	c = a+b;  // Resultado de c -> 15
+	
+	printf("A soma de %d + %d = %d", a,b,c);
+	
+	return 0;
+}
