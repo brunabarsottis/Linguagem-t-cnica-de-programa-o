0910af01bb74b0ff7166bd6cc45d19e314ca5601
brunabarsottis <brunabarsottis@gmail.com>
2026-08-18 08:17:30 -0300

    Lista1.c

--- Lista1.c
+++ Lista1.c
@@ -2,14 +2,26 @@
 #include <stdlib.h>
 #include <math.h>
 
-// Exercicio 3 da lista 1
-
 int main(int argc, char *argv[]) {
+
+// Exercicio 1 da lista 1
+
+	int v1, v2;
+	
+	printf("Insira um numero inteiro: ");
+	scanf("%d", &v1);
+	printf("Insira outro numero inteiro: ");
+	scanf("%d", &v2);
+	
+	printf("A ordem inversa dos numeros lidos: %d e %d", v2, v1);
+
+
+// Exercicio 3 da lista 1
 	
 	
 	int n, resultado, bit64, bit32, bit16, bit8, bit4, bit2 ;
 	
-	printf("Entre com o valor de N: ");
+	printf("\nEntre com o valor de N: ");
 	scanf ("%d", &n);
 	
 	bit64 = n%2;
@@ -25,7 +37,7 @@ int main(int argc, char *argv[]) {
 	bit2 = resultado%2;
 	resultado = resultado/2;
 	
-	printf("O numero %d em binario = %d%d%d%d%d%d%d", n, resultado%2, bit2, bit4, bit8, bit16, bit32, bit64);
+	printf("\nO numero %d em binario = %d%d%d%d%d%d%d", n, resultado%2, bit2, bit4, bit8, bit16, bit32, bit64);
 	
 	
 // Exercicio 8 da lista 1
@@ -33,7 +45,7 @@ int main(int argc, char *argv[]) {
 	int x1,x2,y1,y2;
 	float dist, cat1, cat2;
 	
-	printf("Entre com os valores para p1(x1,y1)");
+	printf("\nEntre com os valores para p1(x1,y1)");
 	scanf("%d", &x1);
 	scanf("%d", &y1);
 	printf("Entre com os valores para p2(x2,y2)");
@@ -46,7 +58,6 @@ int main(int argc, char *argv[]) {
 	dist = sqrt(cat1+cat2);
 	
 	printf("Distancia: %f", dist);
-	
 	
 	return 0;
 }
