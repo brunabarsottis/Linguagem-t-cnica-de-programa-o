42ef6e55548ab389af6cd14ac03a58c8c8cf19fc
brunabarsottis <brunabarsottis@gmail.com>
2026-08-30 20:39:45 -0300

    ExAula.c

--- ExAula.c
+++ ExAula.c
@@ -8,23 +8,33 @@ int main(int argc, char *argv[]) {
 	int a, b, r;
 	printf("Entre com os valores do problema: ");
 	scanf("%d %d", &a, &b);
-	if (a>0 && b>0){
-		if(a%2 == 0 && b%2 == 0){
-			printf("Sao multiplos de 2");
-		}
-		if (a>b){
-			if (a%b == 0){
-				printf("\nsao multiplos");
-		} else {
-			if (b%a ==0){
-				printf("\nsao multiplos");
-				}
-			}
-		} else {
-					
-	}
-}
+	if (a > 0 && b > 0) {
+        if (a % 2 == 0 && b % 2 == 0) {
+            printf("\nAmbos sao multiplos de 2");
+        } else if (a % 2 == 0) {
+            printf("\nApenas %d eh multiplo de 2", a);
+        } else if (b % 2 == 0) {
+            printf("\nApenas %d eh multiplo de 2", b);
+        } else {
+            printf("\nNenhum eh multiplo de 2");
+        }
 
-	
-	return 0;
+        if (a % b == 0 || b % a == 0) {
+            printf("\n%d e %d sao multiplos entre si", a, b);
+        } else {
+            printf("\n%d e %d nao sao multiplos entre si", a, b);
+        }
+    } else {
+        if (a > b) {
+            printf("\nO maior valor eh: %d", a);
+        } else if (b > a) {
+            printf("\nO maior valor eh: %d", b);
+        } else {
+            printf("\nOs valores sao iguais %d", a);
+        }
+    }
+
+    return 0;
 }
+		 
+	
